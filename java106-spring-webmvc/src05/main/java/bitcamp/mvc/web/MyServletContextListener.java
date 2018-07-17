5f657788ff8b02829dc4fa76faa834c9dcc00f2c
maeguni <maeguni@naver.com>
2018-07-17 21:02:34 +0900

    0717

---
+++ java106-spring-webmvc/src05/main/java/bitcamp/mvc/web/MyServletContextListener.java
@@ -0,0 +1,23 @@
+package bitcamp.mvc.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyServletContextListener implements ServletContextListener {
+    
+  
+    public void contextInitialized(ServletContextEvent sce) {
+        
+        System.out.println("contextInitialized()....");
+     }
+    
+
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+        System.out.println("contextInitialized()....");
+    }
+    
+    
+}
