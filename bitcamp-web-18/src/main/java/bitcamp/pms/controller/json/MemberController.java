7e1fb018c31eeac2b9db7af891d70df28d7ca845
maeguni <maeguni@naver.com>
2018-07-30 20:22:40 +0900

    0730

--- bitcamp-web-18/src/main/java/bitcamp/pms/controller/json/MemberController.java
+++ bitcamp-web-18/src/main/java/bitcamp/pms/controller/json/MemberController.java
@@ -45,28 +45,36 @@ public class MemberController {
     }
     
     @PostMapping("add")
-    public String add(Member member) throws Exception {
-        
+    public Object add(Member member) throws Exception {
+        HashMap<String,Object> result = new HashMap<>();
         memberService.add(member);
-        return "redirect:list";
+        result.put("status","success");        
+        return result;
     }
     
     @RequestMapping("delete")
-    public String delete(String id) throws Exception {
-        
-        memberService.delete(id); 
-        return "redirect:list";
-       
-    }
-    
-    @RequestMapping("update")
-    public String update(Member member) throws Exception {
-        
-        if (memberService.update(member) == 0) {
-            return "member/updatefail";
+    public Object delete(String id) throws Exception {
+        HashMap<String,Object> result = new HashMap<>();
+        if (memberService.delete(id) == 0) {
+            result.put("status","fail");
+            result.put("error", "해당 아이디가 없습니다.");
         } else {
-            return "redirect:list";
+            result.put("status","success");
         }
+        return result;
+    }
+       
+ 
+    @RequestMapping("update")
+    public Object update(Member member) throws Exception {
+        HashMap<String,Object> result = new HashMap<>();
+        if (memberService.update(member) == 0) {
+            result.put("status","fail");
+            result.put("error", "해당 아이디가 없습니다.");
+        } else {
+            result.put("status","success");
+        }
+        return result;
     }
     
     @RequestMapping("view/{id}")
