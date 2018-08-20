4f9a3e9c30225ca9697a05f0da0859fd73782f1b
maeguni <maeguni@naver.com>
2018-08-20 19:20:33 +0900

    0820

--- bitcamp-assignment-01/docs/dbmodel/ddl.sql
+++ bitcamp-assignment-01/docs/dbmodel/ddl.sql
@@ -6,70 +6,70 @@ DROP TABLE IF EXISTS P2_BIZCARD RESTRICT;
 
 -- 회원
 CREATE TABLE P2_MEMB (
-	MNO   INTEGER      NOT NULL COMMENT '회원번호', -- 회원번호
-	EMAIL VARCHAR(40)  NOT NULL COMMENT '이메일', -- 이메일
-	NAME  VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
-	PWD   VARCHAR(255) NOT NULL COMMENT '암호' -- 암호
+    MNO   INTEGER      NOT NULL COMMENT '회원번호', -- 회원번호
+    EMAIL VARCHAR(40)  NOT NULL COMMENT '이메일', -- 이메일
+    NAME  VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
+    PWD   VARCHAR(255) NOT NULL COMMENT '암호' -- 암호
 )
 COMMENT '회원';
 
 -- 회원
 ALTER TABLE P2_MEMB
-	ADD CONSTRAINT PK_P2_MEMB -- 회원 기본키
-		PRIMARY KEY (
-			MNO -- 회원번호
-		);
+    ADD CONSTRAINT PK_P2_MEMB -- 회원 기본키
+        PRIMARY KEY (
+            MNO -- 회원번호
+        );
 
 -- 회원 유니크 인덱스
 CREATE UNIQUE INDEX UIX_P2_MEMB
-	ON P2_MEMB ( -- 회원
-		EMAIL ASC -- 이메일
-	);
+    ON P2_MEMB ( -- 회원
+        EMAIL ASC -- 이메일
+    );
 
 -- 회원 인덱스
 CREATE INDEX IX_P2_MEMB
-	ON P2_MEMB( -- 회원
-		NAME ASC -- 이름
-	);
+    ON P2_MEMB( -- 회원
+        NAME ASC -- 이름
+    );
 
 ALTER TABLE P2_MEMB
-	MODIFY COLUMN MNO INTEGER NOT NULL AUTO_INCREMENT COMMENT '회원번호';
+    MODIFY COLUMN MNO INTEGER NOT NULL AUTO_INCREMENT COMMENT '회원번호';
 
 -- 명함
 CREATE TABLE P2_BIZCARD (
-	BCNO  INTEGER     NOT NULL COMMENT '명함번호', -- 명함번호
-	NAME  VARCHAR(50) NOT NULL COMMENT '이름', -- 이름
-	MTEL  VARCHAR(30) NOT NULL COMMENT '휴대전화', -- 휴대전화
-	TEL   VARCHAR(30) NULL     COMMENT '일반전화', -- 일반전화
-	FAX   VARCHAR(30) NULL     COMMENT '팩스', -- 팩스
-	EMAIL VARCHAR(40) NULL     COMMENT '이메일', -- 이메일
-	MEMO  TEXT        NULL     COMMENT '메모', -- 메모
-	MNO   INTEGER     NULL     COMMENT '회원번호' -- 회원번호
+    BCNO  INTEGER     NOT NULL COMMENT '명함번호', -- 명함번호
+    NAME  VARCHAR(50) NOT NULL COMMENT '이름', -- 이름
+    MTEL  VARCHAR(30) NOT NULL COMMENT '휴대전화', -- 휴대전화
+    TEL   VARCHAR(30) NULL     COMMENT '일반전화', -- 일반전화
+    FAX   VARCHAR(30) NULL     COMMENT '팩스', -- 팩스
+    EMAIL VARCHAR(40) NULL     COMMENT '이메일', -- 이메일
+    MEMO  TEXT        NULL     COMMENT '메모', -- 메모
+    MNO   INTEGER     NULL     COMMENT '회원번호' -- 회원번호
 )
 COMMENT '명함';
 
 -- 명함
 ALTER TABLE P2_BIZCARD
-	ADD CONSTRAINT PK_P2_BIZCARD -- 명함 기본키
-		PRIMARY KEY (
-			BCNO -- 명함번호
-		);
+    ADD CONSTRAINT PK_P2_BIZCARD -- 명함 기본키
+        PRIMARY KEY (
+            BCNO -- 명함번호
+        );
 
 -- 명함 인덱스
 CREATE INDEX IX_P2_BIZCARD
-	ON P2_BIZCARD( -- 명함
-		NAME ASC -- 이름
-	);
+    ON P2_BIZCARD( -- 명함
+        NAME ASC -- 이름
+    );
 
 ALTER TABLE P2_BIZCARD
-	MODIFY COLUMN BCNO INTEGER NOT NULL AUTO_INCREMENT COMMENT '명함번호';
+    MODIFY COLUMN BCNO INTEGER NOT NULL AUTO_INCREMENT COMMENT '명함번호';
 
 -- 명함
 ALTER TABLE P2_BIZCARD
-	ADD CONSTRAINT FK_P2_MEMB_TO_P2_BIZCARD -- 회원 -> 명함
-		FOREIGN KEY (
-			MNO -- 회원번호
-		)
-		REFERENCES P2_MEMB ( -- 회원
-			MNO -- 회원번호
-		);
+    ADD CONSTRAINT FK_P2_MEMB_TO_P2_BIZCARD -- 회원 -> 명함
+        FOREIGN KEY (
+            MNO -- 회원번호
+        )
+        REFERENCES P2_MEMB ( -- 회원
+            MNO -- 회원번호
+        );
